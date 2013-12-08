912bffcb1b79c5bbc1ca60240ff48383c4f523b3
hirokiky <hirokiky@gmail.com>
2013-12-08 17:29:20 +0900

    Adding tests for Django1.6 with Python2.7
    * Fixed tox
    * Fixed test_proj

---
+++ test_proj/urls.py
@@ -0,0 +1,14 @@
+try:
+    from django.conf.urls import url, patterns, include
+except ImportError:
+    from django.conf.urls.defaults import url, patterns, include
+from django.conf import settings
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
+    url(r'^admin_tools/', include('admin_tools.urls')),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+)
